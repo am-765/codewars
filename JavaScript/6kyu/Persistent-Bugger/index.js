7d0a9e0f0d2501ef0e21490b90b1f1e16a1d507e
am-765 <zjhghjnlzbghh.765+github@gmail.com>
2021-05-31 23:52:22 +0900

    Make function more short and efficient

--- JavaScript/6kyu/Persistent-Bugger/index.js
+++ JavaScript/6kyu/Persistent-Bugger/index.js
@@ -1,12 +1,4 @@
-const persistence = (num) => {
-  let count = 0;
-  while (num.toString().length > 1) {
-    num = num
-      .toString()
-      .split("")
-      .map(Number)
-      .reduce((ac, cv) => ac * cv);
-    count++;
-  }
-  return count;
-};
+const persistence = (num) =>
+  `${num}`.length > 1
+    ? 1 + persistence(`${num}`.split("").reduce((ac, cv) => ac * cv))
+    : 0;
