e516e83bf540e3cadc161ebb0befb2aa5dcf6c22
am-765 <zjhghjnlzbghh.765+github@gmail.com>
2021-06-06 19:25:24 +0900

    Make function more short and efficient

--- TypeScript/7kyu/Square-Every-Digit/index.ts
+++ TypeScript/7kyu/Square-Every-Digit/index.ts
@@ -1,10 +1,9 @@
 export class Kata {
   static squareDigits(num: number) {
-    return Number(
-      `${num}`
-        .split("")
-        .map((el) => Number(el) ** 2)
-        .join("")
-    );
+    return +num
+      .toString()
+      .split("")
+      .map((el) => (+el) ** 2)
+      .join("");
   }
 }
