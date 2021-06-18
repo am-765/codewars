b859ab17d9585d2e84f24d9c02668cc5a6ac145f
am-765 <zjhghjnlzbghh.765+github@gmail.com>
2021-06-18 10:34:07 +0900

    Add Decode the Morse code

---
+++ TypeScript/6kyu/Decode-the-Morse-code/index.ts
@@ -0,0 +1,36 @@
+// import { MORSE_CODE } from "./preloaded";
+const MORSE_CODE = {
+  ".-": "A",
+  "-...": "B",
+  "-.-.": "C",
+  "-..": "D",
+  ".": "E",
+  "..-.": "F",
+  "--.": "G",
+  "....": "H",
+  "..": "I",
+  ".---": "J",
+  "-.-": "K",
+  ".-..": "L",
+  "--": "M",
+  "-.": "N",
+  "---": "O",
+  ".--.": "P",
+  "--.-": "Q",
+  ".-.": "R",
+  "...": "S",
+  "-": "T",
+  "..-": "U",
+  "...-": "V",
+  ".--": "W",
+  "-..-": "X",
+  "-.--": "Y",
+  "--..": "Z",
+};
+
+export const decodeMorse = (morseCode: string): string =>
+  morseCode
+    .trim()
+    .split(/  | /)
+    .map((el) => MORSE_CODE[el] || " ")
+    .join("");
