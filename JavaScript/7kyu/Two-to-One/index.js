5157b9493bd1e77ddebd3e588f01ee100c5dbbf5
am-765 <zjhghjnlzbghh.765+github@gmail.com>
2022-01-30 02:07:58 +0900

    Add Two to One

---
+++ JavaScript/7kyu/Two-to-One/index.js
@@ -0,0 +1,11 @@
+const longest = (s1, s2) => [...new Set(s1 + s2)].sort().join("");
+
+console.log(longest("aretheyhere", "yestheyarehere"), "aehrsty");
+console.log(
+  longest("loopingisfunbutdangerous", "lessdangerousthancoding"),
+  "abcdefghilnoprstu"
+);
+console.log(
+  longest("inmanylanguages", "theresapairoffunctions"),
+  "acefghilmnoprstuy"
+);
